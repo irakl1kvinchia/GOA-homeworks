6684e1cf526b9f189b9607208072d3bbef9e5678
irakl1kvinchia <kvinchiairakli2@gmail.com>
2024-04-29 20:59:51 +0400

    commit

--- Day 32/homeworks/lesson32.1.py
+++ Day 32/homeworks/lesson32.1.py
@@ -1,1 +1,5 @@
-# Day 32.1
+# Complete the solution so that it returns true if the first argument(string)
+# passed in ends with the 2nd argument (also a string).
+
+# def solution(text, ending):
+#     return text.endswith(ending)
