c3826acbdd5c5c50f864400b4558de9d6e66ef55
irakl1kvinchia <kvinchiairakli2@gmail.com>
2024-05-05 22:02:24 +0400

    commit

---
+++ Day 36/homeworks/lesson36.py
@@ -0,0 +1,20 @@
+# N1
+
+# შექმენით ფუნქცია სახელად manual_reverse, 
+# რომელიც მიიღებს დალაგებულ კოლექციას.
+# თქვენი დავალებაა, რომ შეაბრუნოთ ეს კოლექცია და დააბრუნოთ ამ სახით
+
+# def manual_reverse(number):
+#     reversed_numbers = []
+
+#     for i in number():
+#         number.reverse()
+#         reversed_numbers.append(number)
+    
+#     return reversed_numbers
+
+# print(manual_reverse([1,2,3,4,5,6]))
+
+#_____________________________________________________________________________________________
+
+
