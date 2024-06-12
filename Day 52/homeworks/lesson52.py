4e3a47d97527579ddf66d6b0cb4473714d001508
irakl1kvinchia <kvinchiairakli2@gmail.com>
2024-06-12 23:54:17 +0400

    commit

--- Day 52/homeworks/lesson52.py
+++ Day 52/homeworks/lesson52.py
@@ -1,1 +1,45 @@
 # N1
+
+# Return the number (count) of vowels in the given string.
+# We will consider a, e, i, o, u as vowels for this Kata (but not y).
+# The input string will only consist of lowe
+
+# def get_count(sentence):
+#     vowels = "aeiou"
+#     count = 0
+#     for i in sentence:
+#         if i in vowels:
+#             count += 1
+            
+#     return count
+
+#_____________________________________________________________________________________________________
+
+
+# N2
+
+# Your task is to make a function that can take any 
+# non-negative integer as an argument and return it 
+# with its digits in descending order. Essentially, 
+# rearrange the digits to create the highest possible number.
+
+# def descending_order(num):
+#     num = str(num)
+#     num = list(num)
+    
+#     new_arr = []    
+#     for i in num:
+#         new_arr.append(int(i))
+#     new_arr.sort(reverse = True)
+    
+#     res_str = ""
+#     for x in new_arr:
+#         res_str += str(x)
+#     return int(res_str)
+
+
+#_____________________________________________________________________________________________________
+
+
+# N3
+
