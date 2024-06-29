207ff1ac82bd66662cf1d81ba7bab230cda3d9ad
irakl1kvinchia <kvinchiairakli2@gmail.com>
2024-06-29 17:40:11 +0400

    commi

---
+++ Day 60/classwork/classwork.py
@@ -0,0 +1,27 @@
+#N1
+
+# def reverse_number(n):
+#     n = str(n)
+#     n = n[::-1]
+    
+#     if n[-1] == '-':
+#         n = n[0:-1]
+#         n = "-" + n
+        
+    
+#     return int(n)
+#____________________________________________________________________________________________________
+
+#N2
+
+# def find_average(numbers):
+#     if len(numbers) == 0:
+#         return 0
+    
+#     sum_of_numbers = sum(numbers)
+#     length_of_numbers = len(numbers)
+    
+#     return sum_of_numbers / length_of_numbers
+
+#________________________________________________________________________________________________________
+
